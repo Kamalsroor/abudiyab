3efc081f1cbc5c3b58d138c7ac41354f64bd5898
Yousef-Hatem <yousef26hatem@gmail.com>
2021-04-11 10:42:19 +0200

    new pages

---
+++ resources/views/frontend/points_program.blade.php
@@ -0,0 +1,13 @@
+<x-front-layout :title="trans('dashboard.home')" :breadcrumbs="['dashboard.home']" >
+    <section class="points-program-page">
+
+        <section class="points-program-page_head" style="background: url({{asset('front/img/branches.jpg')}});"><h1>برنامج النطاق</h1></section>
+
+        <section class="points-program-page_center">
+
+            {{-- Content --}}
+
+        </section>
+
+    </section>
+</x-front-layout>
---
+++ resources/views/frontend/services.blade.php
@@ -0,0 +1,13 @@
+<x-front-layout :title="trans('dashboard.home')" :breadcrumbs="['dashboard.home']" >
+    <section class="services-page">
+
+        <section class="services-page_head" style="background: url({{asset('front/img/branches.jpg')}});"><h1>الخدمات والصيانه</h1></section>
+
+        <section class="services-page_center">
+
+            {{-- Content --}}
+
+        </section>
+
+    </section>
+</x-front-layout>
