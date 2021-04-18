192228f647e072a08f6602c6ca17a9e079af3ac6
Yousef-Hatem <yousef26hatem@gmail.com>
2021-04-18 08:51:02 +0200

    fix

--- resources/views/frontend/car_sales.blade.php
+++ resources/views/frontend/car_sales.blade.php
@@ -1,7 +1,15 @@
 <x-front-layout :title="trans('dashboard.home')" :breadcrumbs="['dashboard.home']" >
     <section class="car-sales-page">
 
-        <section class="car-sales-page_head" style="background: url({{optional(Settings::instance('car_sales_backgraund'))->getFirstMediaUrl('car_sales_backgraund')}});background-repeat: no-repeat; background-size: cover;"><h1>{{Settings::locale(app()->getLocale())->get('car_sales_title')}}</h1></section>
+        <section class="car-sales-page_head" style="background: url({{optional(Settings::instance('car_sales_backgraund'))->getFirstMediaUrl('car_sales_backgraund')}});background-repeat: no-repeat; background-size: cover;"><h1>صفحة بيع السيارات</h1></section>
+
+        <section class="car-sales-page_head" >
+            <img class="w-100" src="{{optional(Settings::instance('car_sales_backgraund'))->getFirstMediaUrl('car_sales_backgraund')}}" alt="back-ground">
+
+
+            <h1>{{Settings::locale(app()->getLocale())->get('car_sales_title')}}</h1>
+
+        </section>
 
         <section class="car-sales-page_center">
 
