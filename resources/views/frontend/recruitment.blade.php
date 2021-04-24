b1159b45437ec3b93ab3f3fc50bc696e50495607
Yousef-Hatem <yousef26hatem@gmail.com>
2021-04-24 11:42:08 +0200

    flx

--- resources/views/frontend/recruitment.blade.php
+++ resources/views/frontend/recruitment.blade.php
@@ -16,7 +16,7 @@
                     @csrf
                         <div class="select">
                             <label for="">اختر الوظيفه المناسبه <span>*</span></label>
-                            <select id="" name='jobname' >
+                            <select id="" name='jobname' @if($errors->has('jobname')) class="error" @endif>
                                 <option value="" hidden>وظيفتي هيا</option>
                                 @foreach ($works as $work)
                                     @if ($work->available)
@@ -30,42 +30,42 @@
                         @endif
                         <div class="input">
                             <label for="">الاسم <span>*</span></label>
-                            <input  type="text" id="" name='name' placeholder="الاسم">
+                            <input @if($errors->has('name')) class="error" @endif type="text" id="" name='name' placeholder="الاسم">
                         </div>
                         @if($errors->has('name'))
                             <div class="valiadtion-error">{{ $errors->first('name') }}</div>
                         @endif
                         <div class="input">
                             <label for="">رقم الجوال <span>*</span></label>
-                            <input  type="number" id="" name='phone' placeholder="رقم الجوال">
+                            <input @if($errors->has('phone')) class="error" @endif type="number" id="" name='phone' placeholder="رقم الجوال">
                         </div>
                         @if($errors->has('phone'))
                             <div class="valiadtion-error">{{ $errors->first('phone') }}</div>
                         @endif
                         <div class="input">
                             <label for="">البريد الالكتروني <span>*</span></label>
-                            <input  type="email" id="" name='email' placeholder="البريد الالكتروني">
+                            <input @if($errors->has('email')) class="error" @endif type="email" id="" name='email' placeholder="البريد الالكتروني">
                         </div>
                         @if($errors->has('email'))
                             <div class="valiadtion-error">{{ $errors->first('email') }}</div>
                         @endif
                         <div class="input">
                             <label for="">الراتب المتوقع <span>*</span></label>
-                            <input  type="number" id="" name='expected_salary' placeholder="الراتب المتوقع">
+                            <input @if($errors->has('expected_salary')) class="error" @endif type="number" id="" name='expected_salary' placeholder="الراتب المتوقع">
                         </div>
                         @if($errors->has('expected_salaray'))
                             <div class="valiadtion-error">{{ $errors->first('expected_salaray') }}</div>
                         @endif
                         <div class="input">
                             <label for="">السيرة الذاتية <span>*</span></label>
-                            <input  type="file" id="" name='cv' placeholder="السيرة الذاتية">
+                            <input @if($errors->has('cv')) class="error" @endif type="file" id="" name='cv' placeholder="السيرة الذاتية">
                         </div>
                         @if($errors->has('cv'))
                             <div class="valiadtion-error">{{ $errors->first('cv') }}</div>
                         @endif
                         <div class="textarea">
                             {{-- <label for="">ملاحظات <span>*</span></label> --}}
-                            <textarea   id="" name='message' placeholder="رسالتك"></textarea>
+                            <textarea id="" name='message' placeholder="رسالتك"></textarea>
                         </div>
                         <div class="button">
                             <button class="primary-btn" type="submit">ارسال</button>
