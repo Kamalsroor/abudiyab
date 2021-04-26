96340276d7ee30cef958d34e7770c28acd1da440
kamalsroor <teko.2031@gmail.com>
2021-04-26 10:47:50 +0200

    fix

--- resources/views/dashboard/sliders/partials/form.blade.php
+++ resources/views/dashboard/sliders/partials/form.blade.php
@@ -5,7 +5,7 @@
 {{ BsForm::text('second_header') }}
 @endBsMultilangualFormTabs
 <div class="col-md-12">
-    {{ BsForm::checkbox('is_mobile')->value(1)->withoutDefault()->checked(false) }}
+    {{ BsForm::checkbox('is_mobile')->value(1)->withoutDefault()->checked(isset($slider) ? $slider->is_mobile : false) }}
 </div>
 @isset($slider)
     {{ BsForm::image('image')->files($slider->getMediaResource()) }}
