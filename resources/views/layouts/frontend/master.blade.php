01f03b8369a66fd8168ca89047400542d06ce92d
laratrist <kamal.s.sroor@gmail.com>
2021-05-29 13:12:49 +0200

    fix error

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -684,8 +684,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
         let BookingModelURl = "{{route('front.bookingModel')}}";
         let csrf_token = "{{ csrf_token() }}";
         function openBookingModel(id) {
-            window.livewire.emit('setCarId',id);
+            console.log('openBookingModel : ' + id);
+
+            $("#BookingModel").find('#bookingButton').attr("disabled", true);
             $('#BookingModel').modal('show');
+
+
+            console.log($("#BookingModel").find('#bookingButton'));
+            window.livewire.emit('setCarId',id);
+            setTimeout(() => {
+                $("#BookingModel").find('#bookingButton').attr("disabled", false);
+            }, 2500);
         }
         function cancel(){
             $('.fleet-popup').css('display','none');
